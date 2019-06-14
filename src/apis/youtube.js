7bd0f8b8e83c6efc0f37d078b1e2dbd6d1f549a3
Kritapas Nimtrakul <kritapas333@hotmail.com>
2019-06-14 00:26:21 -0400

    fix bug

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY = process.env.KEY;
+const KEY = "AIzaSyCGA_L5b-_2ExX5OMyy-TH8M7olyzFQfvU";
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
