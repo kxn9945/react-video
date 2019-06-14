b276d365ac131694bf9d0e893bbc8b665b28e0da
Kritapas Nimtrakul <kritapas333@hotmail.com>
2019-06-13 23:14:41 -0400

    Initial

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const KEY = process.env.KEY,
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY
+  }
+});
